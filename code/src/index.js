3a044195e97adf92cfd24b5d02ff88688f4667d1
Sunpreet Sandhu <sunpreetsandhu2002@gmail.com>
2023-05-31 15:38:16 -0400

    Used tachyons to style card component

--- code/src/index.js
+++ code/src/index.js
@@ -3,10 +3,14 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import reportWebVitals from "./reportWebVitals";
 import Card from "./Card";
+import "tachyons";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
+    <Card />
+    <Card />
+    <Card />
     <Card />
   </React.StrictMode>
 );
