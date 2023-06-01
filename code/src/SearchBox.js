b2dd4c714875c11d0116cc0a6d98dbbfeddbf1f8
Sunpreet Sandhu <sunpreetsandhu2002@gmail.com>
2023-06-01 15:41:02 -0400

    Added light blue background for search box

--- code/src/SearchBox.js
+++ code/src/SearchBox.js
@@ -4,7 +4,7 @@ const SearchBox = () => {
   return (
     <div className="pa2">
       <input
-        className="pa3 ba b--green"
+        className="pa3 ba b--green bg-lightest-blue"
         type="search"
         placeholder="search robots"
       />
