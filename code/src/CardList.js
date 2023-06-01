cbabbb3eb6030da03535db8b0ed1b8121d77f8bc
Sunpreet Sandhu <sunpreetsandhu2002@gmail.com>
2023-06-01 12:45:40 -0400

    Passed destructured robots as prop to cardlist

--- code/src/CardList.js
+++ code/src/CardList.js
@@ -1,8 +1,7 @@
 import React from "react";
 import Card from "./Card";
-import { robots } from "./robots";
 
-const CardList = () => {
+const CardList = ({ robots }) => {
   return (
     <div>
       <Card id={robots[0].id} name={robots[0].name} email={robots[0].email} />
--- code/src/index.js
+++ code/src/index.js
@@ -4,11 +4,12 @@ import "./index.css";
 import reportWebVitals from "./reportWebVitals";
 import CardList from "./CardList";
 import "tachyons";
+import { robots } from "./robots";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <CardList />
+    <CardList robots={robots} />
   </React.StrictMode>
 );
 
